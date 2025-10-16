d3345a4579df73e459df4afd177abb627242eb76
mauriciocastillosolis77-source <mauriciocastillosolis77@gmail.com>
2025-10-16 12:35:44 -0600

    Refactor Layout component to simplify props definition
    change 1

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,22 +1,16 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
-interface LayoutProps {
-    children: ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex h-screen bg-gray-900 text-gray-100">
+    <div className="flex h-screen bg-gray-900">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <div className="flex flex-col flex-1 overflow-y-auto">
+      <div className="flex flex-col flex-1">
         <Header setSidebarOpen={setSidebarOpen} />
-        <main className="flex-1 p-4 lg:p-6">
-          {children}
-        </main>
+        <main className="flex-1 overflow-y-auto p-4">{children}</main>
       </div>
     </div>
   );
