4aa92e3fde1885e9c3f068d0259b25dbf469bc59
mauriciocastillosolis77-source <mauriciocastillosolis77@gmail.com>
2026-05-15 21:37:49 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+ 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
@@ -11,11 +11,6 @@ export default defineConfig(({ mode }) => {
         allowedHosts: true,
         hmr: {
           clientPort: 443,
-        },
-        // Requerido para FFmpeg.wasm (SharedArrayBuffer)
-        headers: {
-          'Cross-Origin-Opener-Policy': 'same-origin',
-          'Cross-Origin-Embedder-Policy': 'require-corp',
         },
       },
       envPrefix: ['VITE_', 'GEMINI_'],
